1b340c7eb125bc00aa95831a366b437a3137332f
ikede-divine-keno <ikededivine@gmail.com>
2025-03-27 07:59:34 +0100

    Agent and User Signup and login form password fields upgraded with eye icon to toggle password visibility for better UX

--- src/components/user/FormField.jsx
+++ src/components/user/FormField.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
+
 export default function FormField({ label, placeholder, type="text",  register, name, errors }) {
+    const [showPassword, setShowPassword] = useState(false);
+    const isTypePassword = type === "password"; //Check if Form Field is of type password
 
     return (
-      <div className="mb-5">
+      <div className="mb-5 relative">
         <label className="block text-lg font-semibold text-green-700 mb-1">{label}</label>
   
         <input
-            type={type}
+            type={isTypePassword && showPassword ? "text" : type}
             {...register(name)}
-            className="w-full border border-gray-500 rounded p-2 lg:placeholder:text-lg placeholder:text-black lg:h-16 bg-transparent"
+            className="w-full border border-gray-500 rounded p-2 lg:placeholder:text-lg lg:h-16 bg-transparent"
             placeholder={placeholder}
         />
+
+        {isTypePassword &&
+            <span className="absolute right-4 top-1/2 mt-4 transform -translate-y-1/2 cursor-pointer" onClick={() =>{setShowPassword((prev) => (!prev))}}>
+                {showPassword ? <Eye size={20} />: <EyeOff size={20} />}
+            </span>
+        }
   
         {/* Show validation error */}
         {errors[name] && <p className="text-red-500 text-sm mt-1">{errors[name]?.message}</p>}
